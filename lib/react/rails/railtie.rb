17bb35478bea405cba62095db29d6f9c4680b87e
Moshe Bildner <moshe.bildner@gmail.com>
2016-03-16 11:18:15 -0400

    Update railtie.rb

--- lib/react/rails/railtie.rb
+++ lib/react/rails/railtie.rb
@@ -91,11 +91,8 @@ module React
       initializer "react_rails.setup_engine", :group => :all do |app|
         sprockets_env = app.assets || Sprockets # Sprockets 3.x expects this in a different place
         if Gem::Version.new(Sprockets::VERSION) >= Gem::Version.new("3.0.0")
-          sprockets_env.register_mime_type("application/jsx", extensions: [".jsx", ".js.jsx", ".es.jsx", ".es6.jsx"])
+          sprockets_env.register_mime_type("application/jsx", extensions: [".js", ".jsx", ".js.jsx", ".es.jsx", ".es6.jsx"])
           sprockets_env.register_transformer("application/jsx", "application/javascript", React::JSX::Processor)
-          
-          sprockets_env.register_mime_type("application/javascript", extensions: [".js"])
-          sprockets_env.register_transformer("application/javascript", "application/javascript", React::JSX::BabelTransformer)
         else
           sprockets_env.register_engine(".jsx", React::JSX::Template)
         end
